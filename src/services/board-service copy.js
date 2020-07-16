9d784638e711779a0e1eb8bfdd86f90c716fac27
Asia Vaks <asiavaks@gmail.com>
2020-07-16 17:15:10 +0300

    board service copy2

--- src/services/board-service copy.js
+++ src/services/board-service copy.js
@@ -1,34 +1,48 @@
-var gColumns = [
-    [ { columnTitle: 'test1' , cards:
-        [{ color: 'blue', id: 1, taskTitle: 'Test', taskContent: 'This is a test' },
-        { color: 'yellow', id: 2, taskTitle: 'Test2', taskContent: 'This is a test2' },
-        { color: 'red', id: 3, taskTitle: 'Test3', taskContent: 'This is a test3' },
-        { color: 'purple', id: 4, taskTitle: 'Test4', taskContent: 'This is a test4' },
-        { color: 'green', id: 5, taskTitle: 'Test5', taskContent: 'This is a test5' }]}
-    ],
 
-    [
-        { columnTitle: 'test1' , cards:
-        [{ color: 'blue', id: 6, taskTitle: 'Test', taskContent: 'This is a test' },
-        { color: 'yellow', id: 7, taskTitle: 'Test7', taskContent: 'This is a test2' },
-        { color: 'red', id: 8, taskTitle: 'Test8', taskContent: 'This is a test3' },
-        { color: 'purple', id: 9, taskTitle: 'Test9', taskContent: 'This is a test4' },
-        { color: 'green', id: 10, taskTitle: 'Test10', taskContent: 'This is a test5' }]}
-    ],
+module.exports = {
+    query,
+    getById,
+    remove,
+    save
+}
 
-    [
-        { columnTitle: 'test1' , cards:
-        [{ color: 'blue', id: 11, taskTitle: 'Test11', taskContent: 'This is a test11' },
-        { color: 'yellow', id: 12, taskTitle: 'Test12', taskContent: 'This is a test12' },
-        { color: 'red', id: 13, taskTitle: 'Test13', taskContent: 'This is a test13' },
-        { color: 'purple', id: 14, taskTitle: 'Test14', taskContent: 'This is a test14' },
-        { color: 'green', id: 15, taskTitle: 'Test15', taskContent: 'This is a test15' }]}
-    ]
-]
+function query() {
+    return Promise.resolve(gBoards);
+}
+
+function getById(id) {
+    const board = gBoards.find(gBoard => gBoard._id === id)
+    if(board) return Promise.resolve(gBoard);
+    return Promise.reject('Board could not find');
+}
+
+function remove(id) {
+    const idx = gBoards.findIndex(gBoard => gBoard._id === id)
+    if (idx >= 0) gBoards.splice(idx, 1)
+    return Promise.resolve();
+}
+
+function save(board) {
+    if (board._id) {
+        const idx = gBoards.findIndex(currBoard => currBoard._id === board._id)
+        gBoards.splice(idx, 1, board)
+    } else {
+        board._id = _makeId()
+        gBoards.unshift(board)
+    }
+    return Promise.resolve();
+}
 
 
+// function _saveBoardsToFile() {
+//     fs.writeFileSync('data/gBoard.json', JSON.stringify(gBoards, null, 2));
+// }
 
-
-function getColumns() {
-    return gColumns
+function _makeId(length = 5) {
+    var txt = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (var i = 0; i < length; i++) {
+        txt += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return txt;
 }
