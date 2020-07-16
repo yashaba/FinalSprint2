a1c497d8fcf333f948b1f8333df021724b122279
Asia Vaks <asiavaks@gmail.com>
2020-07-16 18:13:06 +0300

    board service

--- src/services/board-service copy.js
+++ src/services/board-service copy.js
@@ -1,3 +1,75 @@
+let gBoard = {
+    id: "s1001",
+    createdBy: {
+    },
+    activites: [{ activity }, { activity }],
+    members: [],
+    taskGroups: [
+        {
+            id: "s1001",
+            title: "Important",
+            style: {
+                bgColor: green
+            },
+            addedBy,
+            tasks: [
+                {
+                    id: "a101",
+                    by: "minimaluser",
+                    type: "DELETE_TASK",
+                    task: 'minimalTask',
+                    taskGroup: 'minimalGroup',
+                    at: 112223,
+                },
+                {
+                    id: "a102",
+                    by: "minimaluser",
+                    type: "DELETE_TASK",
+                    task: 'minimalTask',
+                    taskGroup: 'minimalGroup',
+                    at: 112223,
+                },
+                {
+                    id: "a103",
+                    by: "minimaluser",
+                    type: "DELETE_TASK",
+                    task: 'minimalTask',
+                    taskGroup: 'minimalGroup',
+                    at: 112223,
+                }],
+        },
+        {
+            title: "Frontend",
+            id: "s1002",
+            color: "green",
+            addedBy: {},
+            tasks: [
+                {
+                    id: "a104",
+                    by: "minimaluser",
+                    type: "DELETE_TASK",
+                    task: 'minimalTask',
+                    taskGroup: 'minimalGroup',
+                    at: 112223,
+                },
+                {
+                    id: "a105",
+                    by: "minimaluser",
+                    type: "DELETE_TASK",
+                    task: 'minimalTask',
+                    taskGroup: 'minimalGroup',
+                    at: 112223,
+                },
+                {
+                    id: "a106",
+                    by: "minimaluser",
+                    type: "DELETE_TASK",
+                    task: 'minimalTask',
+                    taskGroup: 'minimalGroup',
+                    at: 112223,
+                }]
+        }]
+}
 
 module.exports = {
     query,
@@ -7,18 +79,20 @@ module.exports = {
 }
 
 function query() {
-    return Promise.resolve(gBoards);
+    return Promise.resolve(gBoard);
 }
 
 function getById(id) {
-    const board = gBoards.find(gBoard => gBoard._id === id)
-    if(board) return Promise.resolve(gBoard);
+    const taskGroups = gBoard.taskGroups
+    const group = taskGroups.find(group => group._id === id)
+    if (group) return Promise.resolve(group);
     return Promise.reject('Board could not find');
 }
 
 function remove(id) {
-    const idx = gBoards.findIndex(gBoard => gBoard._id === id)
-    if (idx >= 0) gBoards.splice(idx, 1)
+    const taskGroups = gBoard.taskGroups
+    const idx = taskGroups.findIndex(group => group._id === id)
+    if (idx >= 0) taskGroups.splice(idx, 1)
     return Promise.resolve();
 }
 
