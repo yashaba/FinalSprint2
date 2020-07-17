713e7301606fc6010f4b9959e290762ae2db6912
yashaba <yashabazovner@gmail.com>
2020-07-17 17:12:49 +0300

    tttt

--- src/components/taskGroup.vue
+++ src/components/taskGroup.vue
@@ -6,7 +6,7 @@
     <!-- <h1> {{this.columns.columnTitle}}</h1> -->
 <draggable   class="list-group"
         tag="div"   
-        v-bind="dragOptions" v-model="taskGroup.tasks" group="people" @start="drag=true" @end="drag=false">
+        v-bind="dragOptions" v-model="taskGroup.tasks" group="people" @start="drag=true" @end="drag=false, updateBoardEv()">
  <div v-for="task in taskGroup.tasks" :key="task.id">
    <task-preview :task='task'> </task-preview>
    </div>
@@ -55,48 +55,14 @@ export default {
   methods: {
    log() {
      console.log('triggerrr');
+   },
+   updateBoardEv() {
+     console.log('emitted');
+     this.$emit ('updateBoardEv')
    }
   },
 
-  // data() {
-  //   return {
-  //     myArray: [
-        
-  //       { color: 'blue', id: 1 , taskTitle: 'Test' , taskContent: 'This is a test'} ,
-  //       { color: 'yellow', id: 2 , taskTitle: 'Test2' , taskContent: 'This is a test2'} ,
-  //       { color: 'red', id: 3 , taskTitle: 'Test3' , taskContent: 'This is a test3'} ,
-  //       { color: 'purple', id: 4 , taskTitle: 'Test4' , taskContent: 'This is a test4'} ,
-  //       { color: 'green', id: 5 , taskTitle: 'Test5' , taskContent: 'This is a test5'} ,
-    
-   
-      
-  //     ],
-  //     myArray2: [
-        
-  //       { color: 'blue', id: 6 , taskTitle: 'Test' , taskContent: 'This is a test'} ,
-  //       { color: 'yellow', id: 7 , taskTitle: 'Test7' , taskContent: 'This is a test2'} ,
-  //       { color: 'red', id: 8 , taskTitle: 'Test8' , taskContent: 'This is a test3'} ,
-  //       { color: 'purple', id: 9 , taskTitle: 'Test9' , taskContent: 'This is a test4'} ,
-  //       { color: 'green', id: 10 , taskTitle: 'Test10' , taskContent: 'This is a test5'} ,
-    
-   
-      
-  //     ],
-  //     myArray3: [
-        
-  //       { color: 'blue', id: 11 , taskTitle: 'Test11' , taskContent: 'This is a test11'} ,
-  //       { color: 'yellow', id: 12 , taskTitle: 'Test12' , taskContent: 'This is a test12'} ,
-  //       { color: 'red', id: 13 , taskTitle: 'Test13' , taskContent: 'This is a test13'} ,
-  //       { color: 'purple', id: 14 , taskTitle: 'Test14' , taskContent: 'This is a test14'} ,
-  //       { color: 'green', id: 15 , taskTitle: 'Test15' , taskContent: 'This is a test15'} ,
-    
-   
-      
-  //     ],
-  //   }
-  // },
-
-
+ 
 components: {
   draggable,
   taskPreview
